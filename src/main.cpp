98373027463f37861e83109381693098b5f63e92
Bjorn Tipling <bjorn@ambientchill.com>
2016-04-15 07:14:44 -0700

    Set version to OpenGL version to 4.1.

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@ int main(void)
     glfwDefaultWindowHints();
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
